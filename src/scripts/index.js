adfce03d33efdd4781112430787488e2f1abd5bf
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-20 21:54:14 +0300

    Get Input Details When User Clicks Confirm

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,16 +4,28 @@ const addExpenseBtn = document.querySelector('#addExpenseBtn');
 const inputDialog = document.querySelector('#inputDialog');
 const confirmBtn = document.querySelector('#confirm');
 const closeBtn = document.querySelector('#close');
+const categoryInput = document.querySelector('#category');
+const amountInput = document.querySelector('#amount');
+const dateInput = document.querySelector('#date');
+const descriptionInput = document.querySelector('#description');
 
 function showForm() {
   inputDialog.show();
 }
+
 function getInputDetails(event) {
   event.preventDefault();
-}
+  const categoryValue = categoryInput.value;
+  const amountValue = amountInput.value;
+  const dateValue = dateInput.value;
+  const descriptionValue = descriptionInput.value;
+
+  inputDialog.close();
+};
 
 function closeForm(event) {
   event.preventDefault();
+  inputDialog.close();
 }
 addExpenseBtn.addEventListener('click', showForm);
 confirmBtn.addEventListener('click', getInputDetails);
