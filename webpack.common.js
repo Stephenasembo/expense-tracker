7acaa8e7f7ed28c1b3e61d3599988e0379564093
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-20 21:21:17 +0300

    Create Files And File Structure
    The structure will keep our files organized

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/scripts/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/templates/index.html',
     }),
   ],
   module: {
