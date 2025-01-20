0818ec50717cf82f2f9f88f80ff86412b0a1652b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-20 21:37:51 +0300

    Open The Form When Requested By User

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
 import '../styles/styles.css';
+
+const addExpenseBtn = document.querySelector('#addExpenseBtn');
+const inputDialog = document.querySelector('#inputDialog');
+
+function showForm() {
+  inputDialog.show();
+}
+addExpenseBtn.addEventListener('click', showForm);
