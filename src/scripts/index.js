e55e6ccab75178790e3dbd513a9f8c9ce838e529
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-20 21:42:51 +0300

    Prevent Default Form Submissions
    The form isn't being sent to any server hence no need to submit it

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,19 @@ import '../styles/styles.css';
 
 const addExpenseBtn = document.querySelector('#addExpenseBtn');
 const inputDialog = document.querySelector('#inputDialog');
+const confirmBtn = document.querySelector('#confirm');
+const closeBtn = document.querySelector('#close');
 
 function showForm() {
   inputDialog.show();
 }
+function getInputDetails(event) {
+  event.preventDefault();
+}
+
+function closeForm(event) {
+  event.preventDefault();
+}
 addExpenseBtn.addEventListener('click', showForm);
+confirmBtn.addEventListener('click', getInputDetails);
+closeBtn.addEventListener('click', closeForm);
