42e79bdf093db527b83904930c442a6e2d12ba75
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-02-17 23:00:26 +0300

    Feat!: Modularize Code
    Handle Form Operations In Its Own Module

--- src/scripts/form-handling.js
+++ src/scripts/form-handling.js
@@ -1,3 +1,16 @@
+import { domElements, formControls } from './dom';
+
+let userValues = {};
+
+function showForm() {
+  domElements.inputDialog.show();
+}
+
+function closeForm(event) {
+  event.preventDefault();
+  domElements.inputDialog.close();
+}
+
 export default function clientSideValidation(obj) {
   const inputArr = Object.keys(obj);
   for (let i = 0; i < inputArr.length; i += 1) {
@@ -10,3 +23,30 @@ export default function clientSideValidation(obj) {
     }
   }
 }
+
+function getInputDetails(event) {
+  event.preventDefault();
+  const categoryValue = formControls.categoryInput.value;
+  const amountValue = formControls.amountInput.value;
+  const dateValue = formControls.dateInput.value;
+  const descriptionValue = formControls.descriptionInput.value;
+
+  userValues = {
+    categoryValue,
+    amountValue,
+    dateValue,
+    descriptionValue,
+  };
+
+  clientSideValidation(formControls);
+
+  domElements.inputDialog.close();
+}
+
+const setUpButtons = function addListeners() {
+  domElements.addExpenseBtn.addEventListener('click', showForm);
+  formControls.confirmBtn.addEventListener('click', getInputDetails);
+  formControls.closeBtn.addEventListener('click', closeForm);
+};
+
+export { setUpButtons };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,4 @@
 import '../styles/styles.css';
 import { domElements, formControls } from './dom';
 import clientSideValidation from './form-handling';
-
-function showForm() {
-  domElements.inputDialog.show();
-}
-
-let userValues = {};
-
-function getInputDetails(event) {
-  event.preventDefault();
-  const categoryValue = formControls.categoryInput.value;
-  const amountValue = formControls.amountInput.value;
-  const dateValue = formControls.dateInput.value;
-  const descriptionValue = formControls.descriptionInput.value;
-
-  userValues = {
-    categoryValue,
-    amountValue,
-    dateValue,
-    descriptionValue,
-  };
-
-  clientSideValidation(formControls);
-
-  domElements.inputDialog.close();
-}
-
-function closeForm(event) {
-  event.preventDefault();
-  domElements.inputDialog.close();
-}
-domElements.addExpenseBtn.addEventListener('click', showForm);
-formControls.confirmBtn.addEventListener('click', getInputDetails);
-formControls.closeBtn.addEventListener('click', closeForm);
+import { saveExpense, removeExpense, retrieveExpense } from './storage';
