2589e6d5db487fdb4e20a934d86308ed69f28124
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-02-17 23:32:49 +0300

    Feat: Create Expenses
    The expenses are stored in the local storage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,19 @@
 import '../styles/styles.css';
-import { domElements, formControls } from './dom';
-import clientSideValidation from './form-handling';
 import { saveExpense, removeExpense, retrieveExpense } from './storage';
+import { setUpButtons } from './form-handling';
+
+setUpButtons();
+
+const testInput = {
+  categoryValue: 'Food',
+  amountValue: 55,
+  dateValue: '2025-02-28',
+  descriptionValue: 'Pizza and pineapples',
+};
+
+function createExpense(inputObj) {
+  const expenseName = JSON.stringify(inputObj.categoryValue);
+  saveExpense(expenseName, inputObj);
+}
+
+createExpense(testInput);
