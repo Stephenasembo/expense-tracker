915b3aef6a456b603f4da5535403c8eb31de5dbd
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-02-18 09:12:44 +0300

    Feat: Add A Delete Expense Button

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -5,14 +5,22 @@ export default function displayExpense(expenseObj) {
   const categoryPara = document.createElement('p');
   const amountPara = document.createElement('p');
   const datePara = document.createElement('p');
+  const buttonPara = document.createElement('p');
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete Expense';
+  buttonPara.appendChild(deleteBtn);
 
   categoryPara.textContent = expenseObj.categoryValue;
   amountPara.textContent = expenseObj.amountValue;
   datePara.textContent = expenseObj.dateValue;
 
+  newExpenseDiv.appendChild(buttonPara);
   newExpenseDiv.appendChild(categoryPara);
   newExpenseDiv.appendChild(amountPara);
   newExpenseDiv.appendChild(datePara);
+
+  newExpenseDiv.classList.add(expenseObj.categoryValue);
 
   domElements.expenseDiv.appendChild(newExpenseDiv);
 }
